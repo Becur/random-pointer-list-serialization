e3e917f9a4383f88c3ef5904e3c0f9467cb9d9fc
Becur <privezenov96@mail.ru>
2026-04-14 21:54:40 +0300

    Corrected interface

--- src/list.h
+++ src/list.h
@@ -1,15 +1,37 @@
 #pragma once
 
+#include <memory>
 #include <string>
 
 struct ListNode{
     ListNode* prev = nullptr;
     ListNode* next =  nullptr;
+    
     ListNode* rand = nullptr;
-
     std::string data;
-
-    ~ListNode();
 };
 
-bool operator==(const ListNode& lhs, const ListNode& rhs); // For tests
+class List{
+public:
+
+    List() = default;
+
+    List(const List&) = delete;
+    List(List&&) = delete;
+
+    ListNode* AddNode();
+
+    ListNode* Front();
+    const ListNode* Front() const;
+
+    std::uint32_t Size() const;
+
+    ~List();
+
+private:
+    std::unique_ptr<ListNode> front_node;
+    ListNode* back_node = nullptr;
+    std::uint32_t size_ = 0;
+};
+
+bool operator==(const List& lhs, const List& rhs);
--- src/list_serializer.h
+++ src/list_serializer.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <filesystem>
-#include <optional>
 
 #include "list.h"
 
-void SerializeToBin(const ListNode& list, const std::filesystem::path& output_path = "outlet.out");
+void SerializeToBin(const List* list, const std::filesystem::path& output_path);
 
-std::optional<ListNode> DeserializeFromBin(const std::filesystem::path& input_path = "outlet.out"); // For tests
+List* DeserializeFromBin(const std::filesystem::path& input_path); // For tests
 
-std::optional<ListNode> DeserializeFromText(const std::filesystem::path& input_path = "inlet.in");
+List* DeserializeFromText(const std::filesystem::path& input_path);
